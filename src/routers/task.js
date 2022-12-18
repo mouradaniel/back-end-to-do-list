bb3844ea3f414f2b90bd5000ba749f725a442b19
Daniel Moura <daniel-moura@outlook.com>
2022-12-18 10:53:53 -0300

    feat: implement get all tasks and create a new task

---
+++ src/routers/task.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const Task = require("../models/task");
+const router = new express.Router();
+
+router.post("/tasks", async (req, res) => {
+  const task = new Task({ ...req.body });
+
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+module.exports = router;
