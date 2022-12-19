6a565f53b8d9e4bdda7a1148b4d51e2a8178eaee
Daniel Moura <daniel-moura@outlook.com>
2022-12-19 15:55:07 -0300

    feat: implement routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 require('./db/mongoose');
 
+const taskRouter = require('./routers/task');
+
 const app = express();
 
 app.use(express.json());
+app.use(taskRouter);
 
 module.exports = app;
