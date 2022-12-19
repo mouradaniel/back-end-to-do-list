41e96bbd471b2e94e9a18bc0e755d310584f66d5
Daniel Moura <daniel-moura@outlook.com>
2022-12-19 15:55:34 -0300

    feat: implement get a task

--- src/routers/task.js
+++ src/routers/task.js
@@ -22,4 +22,20 @@ router.get("/tasks", async (req, res) => {
   }
 });
 
+router.get("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findOne({ _id });
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
 module.exports = router;
