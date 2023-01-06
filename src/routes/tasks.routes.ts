19d2d4597f01a29936437af7bab9e26ac3d19b28
Daniel Moura <daniel-moura@outlook.com>
2023-01-06 16:32:50 -0300

    feat: implement endpoint list tasks

--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 
 import { createTaskController } from '../useCases/createTask';
+import { listTasksController } from '../useCases/listTasks';
 
 const tasksRoutes = Router();
 
 tasksRoutes.post('/', (req, res) => {
   return createTaskController.handle(req, res);
+});
+
+tasksRoutes.get('/', (req, res) => {
+  return listTasksController.handle(req, res);
 });
 
 export { tasksRoutes };
